92a1852da82daa05a9da1b393b2a494645d71b1c
ST10448265 <zikalalalihle123@gmail.com>
2025-06-16 10:33:03 +0000

    Add files via upload

---
+++ src/loginandsignup/MessageData.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package loginandsignup;
+
+/**
+ *
+ * @author RC_Student_lab
+ */
+class MessageData {
+
+    Object messageID;
+    Object recipient;
+    Object sender;
+    Object content;
+    Object flag;
+    Object messageHash;
+    
+}
